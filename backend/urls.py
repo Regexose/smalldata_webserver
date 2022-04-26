f0068a4755c42b5ade873081f3b14c2b35c46682
staude <stoide@googlemail.com>
2022-04-26 23:36:08 +0200

    add STATIC_URL to django urls

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
+from django.conf.urls.static import static
 from smalldata import views
 
 
@@ -17,4 +19,4 @@ urlpatterns = [
     path('api/categories/<int:pk>/trigger', csrf_exempt(views.trigger_category)),
     path('api/song_state/', views.song_state),
     path("", views.render_react, name="index"),
-]
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
