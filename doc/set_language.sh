46c4d06a4accc385e628f0ecca4f11bd58a586ea
Benjamin Staude <stoide@googlemail.com>
2022-10-12 10:26:53 +0200

    Rebuild set_language to be used as smalldata

--- doc/set_language.sh
+++ doc/set_language.sh
@@ -10,22 +10,27 @@ else
   exit
 fi
 
-cd /home/smalldata/smalldata_webserver || exit
+
+if [ `whoami` != 'smalldata' ]; then
+  echo "This command must be run as user 'smalldata'! Exiting";
+  exit
+fi
+
+
+cd ~/smalldata_webserver || exit
+
 
 # replace language in settings.ini
-sed_str="s/APP_LANGUAGE=[a-z][a-z]/APP_LANGUAGE=$lang/g"
-su - smalldata -c sed -i -E "$sed_str" settings.ini
+sed -i -E s/APP_LANGUAGE=[a-z][a-z]/APP_LANGUAGE=$lang/g ~/smalldata_webserver/settings.ini
 
 # write new .env-file
-su - smalldata -c /home/smalldata/venv/bin/python create_env_file.py
+~/venv/bin/python ~/smalldata_webserver/create_env_file.py
+
 
 # build frontend files
-cd frontend || exit
-su - smalldata -c npm run relocate
+cd frontend
+npm run relocate
 cd ..
 
 # collect all static files
-su - smalldata -c /home/smalldata/venv/bin/python python manage.py collectstatic --noinput
-
-systemctl restart gunicorn
-service nginx restart
+~/venv/bin/python manage.py collectstatic --noinput
--- doc/start_meinungsorgel.sh
+++ doc/start_meinungsorgel.sh
@@ -2,3 +2,5 @@
 
 ln -s /etc/nginx/sites-available/smalldata /etc/nginx/sites-enabled/smalldata
 service nginx reload
+systemctl restart gunicorn
+service nginx restart
