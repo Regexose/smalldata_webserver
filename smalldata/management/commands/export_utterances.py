184b1757d137ef2de8f751f75cb8a8d338ed7b4f
Benjamin Staude <stoide@googlemail.com>
2023-07-03 11:13:47 +0200

    Add from and to arguments to export utterances

--- smalldata/management/commands/export_utterances.py
+++ smalldata/management/commands/export_utterances.py
@@ -2,9 +2,21 @@ import csv
 import os
 from django.core.management.base import BaseCommand
 from smalldata.models import Utterance
+from datetime import datetime
+import argparse
+import pytz
 
 target_dir = 'model_data/db_dumps'
 header = ['Utterance', 'Category', 'Topic', 'Date', 'Time']
+dateformat = "%d.%m.%y-%H:%M"
+
+
+def valid_date(s):
+    try:
+        return pytz.UTC.localize(datetime.strptime(s, dateformat))
+    except ValueError:
+        msg = "not a valid date: {0!r}".format(s)
+        raise argparse.ArgumentTypeError(msg)
 
 
 class Command(BaseCommand):
@@ -12,11 +24,15 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('filename', type=str, help='name of the target file')
+        parser.add_argument('from', type=valid_date, help='Start time (DD.MM.YY-HH:SS, i.e. "23.07.23-08:23")')
+        parser.add_argument('to', type=valid_date, help='End time (DD.MM.YY-HH:SS, i.e. "23.07.23-08:23")')
 
     def handle(self, *args, **kwargs):
         filename = kwargs['filename']
+        start_from = kwargs['from']
+        to =  kwargs['to']
         target = os.path.join(target_dir, filename)
-        utterances = Utterance.objects.all()
+        utterances = Utterance.objects.filter(created__range=(start_from, to))
         with open(target, 'w', encoding='utf-8') as f:
             writer = csv.writer(f)
             writer.writerow(header)
